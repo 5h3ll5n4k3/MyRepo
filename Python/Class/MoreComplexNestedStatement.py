96c33d3bf5cf3cddf52d984dc0195399a023c9a0
B-radfromalibu <brandonstewart101@yahoo.com>
2022-09-06 14:46:27 -0400

    class

--- Python/Class/MoreComplexNestedStatement.py
+++ Python/Class/MoreComplexNestedStatement.py
@@ -1,7 +1,7 @@
 my_list = []
 question = 'y'
 while question != 'n':
-    my_list.append(input("give me an item name:\nl"))
+    my_list.append(input("give me an item name:\n"))
     while True:
         question = input('would you like to add another?\n[Y|n]: ')
         if question in ['Y','n','']:
